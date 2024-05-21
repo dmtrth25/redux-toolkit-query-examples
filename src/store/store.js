89f553383047a95a7de09d60e3f75cf77c417e32
dmtrth25 <vodolazhchenko25@gmail.com>
2024-05-21 16:56:28 +0300

    fix

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,8 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import { reducer as favoritesReducer } from './favorites/favorite.slice'
 import { userSlice } from './user/user.slice'
 import { api } from './api/api'
 
 const reducers = combineReducers({
-  favorites: favoritesReducer,
   user: userSlice.reducer,
   [api.reducerPath]: api.reducer,
 })
